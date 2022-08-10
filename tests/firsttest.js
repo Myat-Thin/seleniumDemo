6a35f0e04b961e5e892fdbd9fca282a2eb295793
Myat <myatthinkhaing1234@gmail.com>
2022-08-10 11:30:04 +0630

    Installing Mocha

--- tests/firsttest.js
+++ tests/firsttest.js
@@ -14,6 +14,14 @@ async function example(){
     await driver.findElement(By.name("password")).sendKeys("password",Key.RETURN) //adding password
 
 
+
+    //to create the orders from sale team
+    await driver.get("http://103.203.132.254:8888/admin/sale-management/create")
+
+    //adding data
+    //await driver.findElement(By.name("townships")).sendKeys("Ahlone",Key.RETURN)
+
+
     //closing the browser
    // await driver.quit()
 }
